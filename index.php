aefb83a2a1eaab2583cab5028fb0035c02c56ba4
Altofi00 <alfitodwiaditya87@gmail.com>
2024-10-05 10:58:02 +0700

    update sidebar

--- index.php
+++ index.php
@@ -21,21 +21,125 @@
 </head>
 
 <body>
-    <!-- Main content -->
-            <!-- Card end -->
-            </div>
-            <!-- Grafik TDS -->
-            <div class="container mt-10 text-center">
-                <h3>Grafik TDS</h3>
-                <p>(Data yang ditampilkan adalah 5 data terakhir)</p>
+    <!-- sidebar start -->
+    <div x-data="setup()" x-init="$refs.loading.classList.add('hidden');">
+        <div class="flex h-screen antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
+            <!-- Loading screen -->
+            <div
+                x-ref="loading"
+                class="fixed inset-0 z-50 flex items-center justify-center text-2xl font-semibold text-white bg-blue-600">
+                Loading.....
             </div>
 
-            <div class="container w-1/3 text-center mt-5" id="responsecontainer">
-                <!-- Grafik data dari tds.php akan dimuat di sini -->
-            </div>
-       
-   
+            <!-- Sidebar -->
+            <div
+                x-transition:enter="transform transition-transform duration-300"
+                x-transition:enter-start="-translate-x-full"
+                x-transition:enter-end="translate-x-0"
+                x-transition:leave="transform transition-transform duration-300"
+                x-transition:leave-start="translate-x-0"
+                x-transition:leave-end="-translate-x-full"
+                x-show="isSidebarOpen"
+                class="fixed inset-y-0 z-10 flex w-80">
+                <!-- Curvy shape -->
+                <svg
+                    class="absolute inset-0 w-full h-full text-white"
+                    style="filter: drop-shadow(10px 0 10px #00000030)"
+                    preserveAspectRatio="none"
+                    viewBox="0 0 309 800"
+                    fill="currentColor"
+                    xmlns="">
+                    <path
+                        d="M268.487 0H0V800H247.32C207.957 725 207.975 492.294 268.487 367.647C329 243 314.906 53.4314 268.487 0Z" />
+                </svg>
+                <!-- Sidebar content -->
+                <div class="z-10 flex flex-col flex-1">
+                    <div class="flex items-center justify-between flex-shrink-0 w-64 p-4">
+                        <!-- Logo -->
+                        <a href="#">
+                            <div class="p-2.5 mt-1 flex items-center">
+                                <img src="assets/img/water-icon.gif" alt="" class="h-8 w-8 rounded-full">
+                                <h1 class="font-bold text-black text-[15px] ml-3">SI | Monitoring Air </h1>
+                            </div>
+                        </a>
+                        <!-- Close btn -->
+                        <button @click="isSidebarOpen = false" class="p-1 rounded-lg focus:outline-none focus:ring">
+                            <svg
+                                class="w-6 h-6"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                            <span class="sr-only">Close sidebar</span>
+                        </button>
+                    </div>
 
+                    <nav class="flex flex-col flex-1 w-64 p-4 mt-4">
+                        <a href="index.php" class="flex items-center space-x-2">
+                            <img src="assets/img/home.png" alt="homepage" class="h-5 w-5 mb-1 ml-4">
+                            <span>Dashboard</span>
+
+                            <a href="#" class="flex items-center space-x-2 mt-5">
+                                <img src="assets/img/bar-chart.png" alt="user" class="h-5 w-5 mb-1 ml-4">
+                                <span>Line Chart</span>
+                            </a>
+                            <a href="#" class="flex items-center space-x-2 mt-5">
+                                <img src="assets/img/contract.png" alt="monitoring" class="h-5 w-5 mb-1 ml-4">
+                                <span>Data</span>
+                            </a>
+                            <a href="#" class="flex items-center space-x-2 mt-5">
+                                <img src="assets/img/setting.png" alt="monitoring" class="h-5 w-5 mb-1 ml-4">
+                                <span>Setting</span>
+                            </a>
+                    </nav>
+                    <div class="flex-shrink-0 p-4">
+                        <button class="flex items-center space-x-2">
+                            <img src="assets/img/logout.png" alt="tombol logout" class="h-5 w-5 mb-1 ml-4">
+                            <span>Logout</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <main class="flex flex-col items-center justify-center flex-1">
+                <!-- Page content -->
+                <button @click="isSidebarOpen = true" class="fixed p-2 text-white bg-black rounded-lg top-5 left-5">
+                    <svg
+                        class="w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    <span class="sr-only">Open menu</span>
+                </button>
+
+                <!-- Grafik TDS -->
+                <div class="container mt-10 text-center">
+                    <h3>Grafik TDS</h3>
+                    <p>(Data yang ditampilkan adalah 5 data terakhir)</p>
+                </div>
+
+                <div class="container w-1/3 text-center mt-5" id="responsecontainer">
+                    <!-- Grafik data dari tds.php akan dimuat di sini -->
+                </div>
+
+            </main>
+        </div>
+    </div>
+    <!-- sidebar end -->
+
+    <script src="./node_modules/alpinejs/dist/cdn.min.js" defer></script>
+    <script>
+        const setup = () => {
+            return {
+                isSidebarOpen: false,
+            }
+        }
+    </script>
 </body>
 
 </html>
--- sidebar.php
+++ sidebar.php
@@ -5,106 +5,119 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/output.css">
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
-    <title>SideBar</title>
+    <title>Document</title>
 </head>
 
-<body class="font-mono">
-<aside class="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] text-center bg-blue-500">
-            <!-- sidebar start -->
-            <div class="text-white-100 text-xl mt-7">
-                <a href="#">
-                    <div class="p-2.5 mt-1 flex items-center">
-                        <img src="assets/img/water-icon.gif" alt="" class="h-8 w-8 rounded-full">
-                        <h1 class="font-bold text-white text-[15px] ml-3">SI | Monitoring Air </h1>
+<body>
+    <!-- component -->
+    <div x-data="setup()" x-init="$refs.loading.classList.add('hidden');">
+        <div class="flex h-screen antialiased text-gray-900 bg-gray-100 dark:bg-dark dark:text-light">
+            <!-- Loading screen -->
+            <div
+                x-ref="loading"
+                class="fixed inset-0 z-50 flex items-center justify-center text-2xl font-semibold text-white bg-blue-600">
+                Loading.....
+            </div>
+
+            <!-- Sidebar -->
+            <div
+                x-transition:enter="transform transition-transform duration-300"
+                x-transition:enter-start="-translate-x-full"
+                x-transition:enter-end="translate-x-0"
+                x-transition:leave="transform transition-transform duration-300"
+                x-transition:leave-start="translate-x-0"
+                x-transition:leave-end="-translate-x-full"
+                x-show="isSidebarOpen"
+                class="fixed inset-y-0 z-10 flex w-80">
+                <!-- Curvy shape -->
+                <svg
+                    class="absolute inset-0 w-full h-full text-white"
+                    style="filter: drop-shadow(10px 0 10px #00000030)"
+                    preserveAspectRatio="none"
+                    viewBox="0 0 309 800"
+                    fill="currentColor"
+                    xmlns="">
+                    <path
+                        d="M268.487 0H0V800H247.32C207.957 725 207.975 492.294 268.487 367.647C329 243 314.906 53.4314 268.487 0Z" />
+                </svg>
+                <!-- Sidebar content -->
+                <div class="z-10 flex flex-col flex-1">
+                    <div class="flex items-center justify-between flex-shrink-0 w-64 p-4">
+                        <!-- Logo -->
+                        <a href="#">
+                            <div class="p-2.5 mt-1 flex items-center">
+                                <img src="assets/img/water-icon.gif" alt="" class="h-8 w-8 rounded-full">
+                                <h1 class="font-bold text-black text-[15px] ml-3">SI | Monitoring Air </h1>
+                            </div>
+                        </a>
+                        <!-- Close btn -->
+                        <button @click="isSidebarOpen = false" class="p-1 rounded-lg focus:outline-none focus:ring">
+                            <svg
+                                class="w-6 h-6"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                            <span class="sr-only">Close sidebar</span>
+                        </button>
                     </div>
-                </a>
-                <hr class="mt-3 mb-3">
 
-            <!-- fitur search start -->
+                    <nav class="flex flex-col flex-1 w-64 p-4 mt-4">
+                        <a href="index.php" class="flex items-center space-x-2">
+                            <img src="assets/img/home.png" alt="homepage" class="h-5 w-5 mb-1 ml-4">
+                            <span>Dashboard</span>
 
-            <form class="max-w-md mx-auto">
-                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                <div class="relative">
-                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
+                            <a href="#" class="flex items-center space-x-2 mt-5">
+                                <img src="assets/img/bar-chart.png" alt="user" class="h-5 w-5 mb-1 ml-4">
+                                <span>Line Chart</span>
+                            </a>
+                            <a href="#" class="flex items-center space-x-2 mt-5">
+                                <img src="assets/img/contract.png" alt="monitoring" class="h-5 w-5 mb-1 ml-4">
+                                <span>Data</span>
+                            </a>
+                            <a href="#" class="flex items-center space-x-2 mt-5">
+                                <img src="assets/img/setting.png" alt="monitoring" class="h-5 w-5 mb-1 ml-4">
+                                <span>Setting</span>
+                            </a>
+                    </nav>
+                    <div class="flex-shrink-0 p-4">
+                        <button class="flex items-center space-x-2">
+                            <img src="assets/img/logout.png" alt="tombol logout" class="h-5 w-5 mb-1 ml-4">
+                            <span>Logout</span>
+                        </button>
                     </div>
-                    <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...." required />
-                    <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                 </div>
-            </form>
-
-            <!-- fitur search end -->
-
-            <!-- dashboard start-->
-            <a href="dashboard.php">
-                <div class="p-2.5 mt-3 flex rounded-md px-4 duration-30
-                            cursor pointer text-white hover:bg-white hover:text-black shadow-lg">
-                    <img src="assets/img/home.png" alt="homepage" class="h-5 w-5 mb-1 ml-4">
-                    <span class="ml-7 text-xl -mt-1">Dashboard</span>
-                </div>
-            </a>
-            <!-- dashboard end -->
-
-            <!-- line chart start -->
-            <a href="lihat-user.php">
-                <div class="p-2.5 mt-3 flex rounded-md px-4 duration-30
-                            cursor pointer text-white hover:bg-white hover:text-black shadow-lg">
-                    <img src="assets/img/bar-chart.png" alt="user" class="h-5 w-5 mb-1 ml-4">
-                    <span class="ml-7 text-xl -mt-1">Line-Chart</span>
-                </div>
-            </a>
-            <!-- line chart end -->
-
-            <!-- pie chart start -->
-            <a href="user.php">
-                <div class="p-2.5 mt-3 flex rounded-md px-4 duration-30
-                            cursor pointer text-white hover:bg-white hover:text-black shadow-lg">
-                    <img src="assets/img/analytics.png" alt="tambah user" class="h-5 w-5 mb-1 ml-4">
-                    <span class="ml-7 text-xl -mt-1">Pie Chart</span>
-                </div>
-            </a>
-            <!-- pie chart end -->
+            </div>
             
-            <!-- data start -->
-            <a href="monitoring.php">
-                <div class="p-2.5 mt-3 flex rounded-md px-4 duration-30
-                            cursor pointer text-white hover:bg-white hover:text-black shadow-lg">
-                    <img src="assets/img/contract.png" alt="monitoring" class="h-5 w-5 mb-1 ml-4">
-                    <span class="ml-7 text-xl -mt-1">Data</span>
-                </div>
-            </a>
-            <!-- data end -->
+            <main class="flex flex-col items-center justify-center flex-1">
+                <!-- Page content -->
+                <button @click="isSidebarOpen = true" class="fixed p-2 text-white bg-black rounded-lg top-5 left-5">
+                    <svg
+                        class="w-6 h-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                    <span class="sr-only">Open menu</span>
+                </button>
 
-            <!-- data start -->
-            <a href="monitoring.php">
-                <div class="p-2.5 mt-3 flex rounded-md px-4 duration-30
-                            cursor pointer text-white hover:bg-white hover:text-black shadow-lg">
-                    <img src="assets/img/setting.png" alt="monitoring" class="h-5 w-5 mb-1 ml-4">
-                    <span class="ml-7 text-xl -mt-1">Setting</span>
-                </div>
-            </a>
-            <!-- data end -->
-
-            <hr class="mb-3 mt-3">
-
-            <!-- logout start -->
-            <a href="login.php">
-                <div class="">
-                <div class="p-2.5 mt-64 flex rounded-md px-4 duration-30
-                            cursor pointer text-white hover:bg-white hover:text-red-700 hover:text-strong shadow-lg">
-                    <img src="assets/img/logout.png" alt="tombol logout" class="h-5 w-5 mb-1 ml-4">
-                    <span class="ml-7 text-xl -mt-1">Logout</span>
-                </div>
-                </div>
-                
-            </a>
-            <!-- logout end -->
+            </main>
         </div>
-        <!-- sidebar end -->
-        </aside>
+    </div>
+
+    <script src="./node_modules/alpinejs/dist/cdn.min.js" defer></script>
+    <script>
+        const setup = () => {
+            return {
+                isSidebarOpen: false,
+            }
+        }
+    </script>
 </body>
 
 </html>
